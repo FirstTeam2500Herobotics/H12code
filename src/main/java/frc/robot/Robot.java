6578ac8c1150c0acd69c02e6f5259d45efe9c7e7
Herobotics <2500herobotics@gmail.com>
2019-03-15 19:32:26 -0500

    Added comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,62 +11,75 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
+ * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
 public class Robot extends TimedRobot {
-	//Things for the dashboard for picking auto
+	// Things for the dashboard for picking auto
 	Command autonomousCommand;
 	SendableChooser<String> autonomousChooser = new SendableChooser<>();
-	
+
 	// public static UsbCamera cam0 = new UsbCamera("cam0", 0);
 	// public static UsbCamera cam1 = new UsbCamera("cam1", 1);
 
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
-		//Adds a box in the dropdown for each auto mode (default auto if none picked is baseline)
+		// Adds a box in the dropdown for each auto mode (default auto if none picked is
+		// baseline)
 		// autonomousChooser.addDefault("Base Line", "Base Line");
 		SmartDashboard.putData("Auto mode", autonomousChooser);
+		// Set up the cammeras
 		CameraServer.getInstance().startAutomaticCapture("cam0", 0);
-		CameraServer.getInstance().startAutomaticCapture("cam1", 1);
+		// CameraServer.getInstance().startAutomaticCapture("cam1", 1);
+
+		// Subsytems are created the first time we get a instance so we are just
+		// creating them now
 		Chassis.getInstance();
 		Panel.getInstance();
 	}
-	
+
 	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
+	 * This function is called once each time the robot enters Disabled mode. You
+	 * can use it to reset any subsystem information you want to clear when the
+	 * robot is disabled.
 	 */
 	@Override
 	public void disabledInit() {
-		
+
 	}
 
+	/**
+	 * This function is called once each time time the robot enters Autonoums.
+	 */
 	@Override
 	public void autonomousInit() {
-		if(autonomousChooser.getSelected() != null){
-			//Check what auto is picked
-			switch(autonomousChooser.getSelected()){
+		// We want to make sure that we dont have problems if we forget to create a
+		// autonomouseChooser in the driver station
+		if (autonomousChooser.getSelected() != null) {
+			// Check what auto is picked
+			switch (autonomousChooser.getSelected()) {
 			// case "Base Line":
-			// 	autonomousCommand = new BaseLine();
+			// autonomousCommand = new BaseLine();
+			case "TeliOp":
+				// If we are doing teliop just let the default command in chassis and everything
+				// else do its thing
+				break;
 			default:
 				System.out.println("No auto picked");
 				break;
 			}
 		}
 
-		//Start running whatever auto is picked
-		if (autonomousCommand != null){
+		// Start running whatever auto is picked if one is picked
+		if (autonomousCommand != null) {
 			autonomousCommand.start();
-		}
-		else{
+		} else {
 			System.out.println("Something went wrong in starting auto");
 		}
 	}
@@ -79,7 +92,7 @@ public class Robot extends TimedRobot {
 	@Override
 	public void teleopInit() {
 		// This makes sure that the autonomous stops running when teleop starts running.
-		if (autonomousCommand != null){
+		if (autonomousCommand != null) {
 			autonomousCommand.cancel();
 		}
 	}
--- src/main/java/frc/robot/driverStation/Controller.java
+++ src/main/java/frc/robot/driverStation/Controller.java
@@ -9,6 +9,8 @@ import frc.robot.subSystems.panel.ToggleExtender;
 
 public class Controller {
 
+	// USB ports that we are using for the differnt ports
+	// Note: CoPilot is not being used right now
 	public static final int PORT_PILOT_CONTROLLER = 0;
 	public static final int PORT_COPILOT_CONTROLLER = 1;
 
@@ -18,12 +20,15 @@ public class Controller {
 	public static Controller instance;
 
 	public static Controller getInstance() {
-		if (instance == null)
+		if (instance == null) {
 			instance = new Controller();
-
+		}
 		return instance;
 	}
 
+	// Supress the waring for resource leaks because we never save the
+	// JoystickButtons in a var
+	@SuppressWarnings("resource")
 	public Controller() {
 		pilot = new Joystick(PORT_PILOT_CONTROLLER);
 		coPilot = new Joystick(PORT_COPILOT_CONTROLLER);
@@ -32,17 +37,10 @@ public class Controller {
 		new JoystickButton(pilot, GamePad.Button.A).whenPressed(new ToggleActuator());
 		new JoystickButton(pilot, GamePad.Button.B).whenPressed(new ToggleExtender());
 
-		// new JoystickButton(pilot, GamePad.Button.Y).whenPressed(new
-		// ToggleBallIntake());
+		new JoystickButton(pilot, GamePad.Button.START).whenPressed(new InvertDrive());
 
 		// new JoystickButton(pilot, GamePad.Button.BACK).whenPressed(new
 		// ToggleCammera());
-
-		new JoystickButton(pilot, GamePad.Button.START).whenPressed(new InvertDrive());
-
-		// JoystickButton CoLB = new JoystickButton(pilot, GamePad.Button.LB);
-		// CoLB.whenPressed(new SetBallIntakeWheels(1));
-		// CoLB.whenReleased(new SetBallIntakeWheels(0));
 	}
 
 	// Function to remove the controller not homing properly
